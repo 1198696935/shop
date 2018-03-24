ccca5a0b71c6cb89a8a4167c5bbdcbd957246820
1198696935 <1198696935@qq.com>
2018-03-24 13:54:43 +0800

    44

--- src/main/java/pojo/Order.java
+++ src/main/java/pojo/Order.java
@@ -1,5 +1,92 @@
 package pojo;
 
-public class Order {
+import java.sql.Date;
 
+public class Order {
+	
+	private Integer oid;
+	private Integer uid;
+	private Integer status;	
+	private Double payment;
+	private Date deliveryTime;
+	private Date receiptTime;
+	private String username;
+	private String address;
+	private String other;
+	private String phone;
+    private String createTime;
+    
+    
+	public Integer getOid() {
+		return oid;
+	}
+	public void setOid(Integer oid) {
+		this.oid = oid;
+	}
+	
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Double getPayment() {
+		return payment;
+	}
+	public void setPayment(Double payment) {
+		this.payment = payment;
+	}
+	
+
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getOther() {
+		return other;
+	}
+	public void setOther(String other) {
+		this.other = other;
+	}
+	
+	public Date getDeliveryTime() {
+		return deliveryTime;
+	}
+	public void setDeliveryTime(Date deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+	public Date getReceiptTime() {
+		return receiptTime;
+	}
+	public void setReceiptTime(Date receiptTime) {
+		this.receiptTime = receiptTime;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	
+	
 }
